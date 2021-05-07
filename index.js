3d5d541cf35e4f2bfbb8dede58e974a551e81aa5
wuyungen <wuyungen@qiyi.com>
2021-05-07 16:30:31 +0800

    [feature] add dist

---
+++ index.js
@@ -0,0 +1,3 @@
+const HighCdnWebpackPlugin = require('./dist/main.bundle')
+
+module.exports = HighCdnWebpackPlugin
--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
-  },
+  }/* ,
   plugins: [new HtmlWebpackPlugin({
     template: 'public/index.html'
   }), new CdnWebpackPlugin({
@@ -32,5 +32,5 @@ module.exports = {
         },
       ],
       css: ['https://cdn.bootcdn.net/ajax/libs/element-ui/2.15.1/theme-chalk/index.min.css']
-  })]
+  })] */
 };
